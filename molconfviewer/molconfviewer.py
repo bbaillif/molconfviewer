ddfb4f91a0530e778e4e1222bbb095b9855cc9f9
benoitba <benoit.baillif@gmail.com>
2022-02-11 15:34:34 +0000

    fix docstrings

--- molconfviewer/molconfviewer.py
+++ molconfviewer/molconfviewer.py
@@ -7,24 +7,24 @@ from rdkit.Chem.rdchem import Mol
 from typing import Tuple
 
 class MolConfViewer():
+    """Class to generate views of molecule conformations
+    
+    :param widget_size: canvas size
+    :type widget_size: tuple(int, int)
+    :param style: type of drawing molecule, see 3dmol.js
+    :type style: str in ['line', 'stick', 'sphere', 'cartoon']
+    :param draw_surface: display SAS
+    :type draw_surface: bool
+    :param opacity: opacity of surface, ranging from 0 to 1
+    :type opacity: float
+    """
     
     def __init__(self, 
                  widget_size: Tuple[int, int] = (300, 300), 
                  style: str = "stick", 
                  draw_surface: bool = False, 
                  opacity: float = 0.5):
-        """
-        Setup the viewer
-        
-        :param widget_size: canvas size
-        :type widget_size: tuple(int, int)
-        :param style: type of drawing molecule, see 3dmol.js
-        :type style: str in ['line', 'stick', 'sphere', 'cartoon']
-        :param draw_surface: display SAS
-        :type draw_surface: bool
-        :param opacity: opacity of surface, ranging from 0 to 1
-        :type opacity: float
-        
+        """Setup the viewer
         """
         self.widget_size = widget_size
         assert style in ('line', 'stick', 'sphere', 'cartoon')
@@ -33,15 +33,13 @@ class MolConfViewer():
         self.opacity = opacity
     
     def view(self, mol: Mol) :
-        """
-        View a RDKit molecule in 3D, with a slider to explore conformations
-        Largely inspired from 
+        """View a RDKit molecule in 3D, with a slider to explore conformations.
+        Largely inspired from
         https://birdlet.github.io/2019/10/02/py3dmol_example/
         
         :param mol: molecule to show conformers for
         :type mol: Mol
         :return: Nothing, prints a jupyter widget to show the molecule
-        
         """
 
         max_conf_id = mol.GetNumConformers() - 1
@@ -55,8 +53,7 @@ class MolConfViewer():
     def get_viewer(self, 
                     mol: Mol, 
                     conf_id: int = -1) -> py3Dmol.view:
-        """
-        Draw a given conformation for a molecule in 3D using 3Dmol.js
+        """Draw a given conformation for a molecule in 3D using 3Dmol.js
         
         :param mol: molecule to show conformers for
         :type mol: Mol
@@ -64,7 +61,6 @@ class MolConfViewer():
         :type conf_id: int
         :return: molecule viewer for given conf_id
         :rtype: py3Dmol.view
-        
         """
         
         mblock = Chem.MolToMolBlock(mol, confId=conf_id)
